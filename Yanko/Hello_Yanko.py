dcebcaea546ca22e022a3a94199b30557a627cb5
Yanko <wereshchagin@gmail.com>
2019-06-27 10:01:18 +0300

    Первое задание

--- Yanko/Hello_Yanko.py
+++ Yanko/Hello_Yanko.py
@@ -1,3 +1,14 @@
 print("hello")
-print("Hello world")
+print("Hello world") 	print("Hello world")
+print ("H"+"2")
+
+ x=input("x=x" +"(y/n)")
+
+ if x=="n":
+    print("no")
+elif x=="y":
+    print("yes")
+else:
+    print("O_o")
+
 print("Hello" +"Home!")
