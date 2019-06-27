58c70bfd2fe1472092d5c6c85ce27e01b360a0a6
Gigteg <wereshchagin@gmail.com>
2019-06-27 10:17:13 +0300

    правка

--- Yanko/Hello_Yanko.py
+++ Yanko/Hello_Yanko.py
@@ -1,11 +1,11 @@
 print("hello")
-print("Hello world") 	print("Hello world")
+print("Hello world")
 print ("H"+"2")
 
- x=input("x=x" +"(y/n)")
- if x=="n":
+x=input("(y/n)")
+if x=="n":
     print("no")
 elif x=="y":
     print("yes")
 else:
-    print("O_o")
+    print("XXXXXX")
