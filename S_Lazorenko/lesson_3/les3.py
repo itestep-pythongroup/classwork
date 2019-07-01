afd9afeb34cc59f30c68799b394d654405b8fdc4
student <Lazor2004@ukr.net>
2019-07-01 21:52:06 +0300

    klaswork

---
+++ S_Lazorenko/lesson_3/les3.py
@@ -0,0 +1,28 @@
+# def step(x):
+#     return x**2
+#     print(x)
+# x=6
+# print(step(x))
+
+# print (type(9.9)==int)
+# print (type(12)==str)
+# print (type("x"))
+
+# def tip(x):
+#     print(x)
+#     if type(x)==(int):
+#         print("int")
+#     elif type(x)==(str):
+#         print("str")
+#     elif type(x) == (float):
+#         print("float")
+# tip("c")
+
+def preoobrazovanie(a,b):
+    x=('a'+'b')
+
+    print(preoobrazovanie())
+
+
+
+
