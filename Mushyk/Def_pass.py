b4e8289aa79e067c8430eb32398ec2d67da211df
Mushyk <mushyl.s.m18@gmail.com>
2019-07-03 20:04:19 +0300

    Ничего не менял2

---
+++ Mushyk/Def_pass.py
@@ -0,0 +1,18 @@
+# def m4():
+#     x=1
+#     return x + 3
+#
+# def m5(name,x):
+#     print(name,"=",x)
+#
+# print("m4=",m4, m4())
+
+# def a(b):
+#     print(b)
+# x=3
+# a(x)
+
+def ul(item1,item2,item3):
+    pass
+def li(item):
+    pass
