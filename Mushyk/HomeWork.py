e6da11bda3d66f80f5b7f38999a19ae0033970bf
Yanko <wereshchagin@gmail.com>
2019-07-03 20:34:39 +0300

    Lesson4

--- Mushyk/HomeWork.py
+++ Mushyk/HomeWork.py
@@ -1,2 +1,6 @@
+<<<<<<< .merge_file_a04176
 print("HomeWork")
 print("Hello")
+=======
+
+>>>>>>> .merge_file_a04088
---
+++ Yanko/lesson_3/Lesson3.py
@@ -0,0 +1,60 @@
+#def print_hello(a,b,c):
+    #x=a*b*c
+    #print(x)
+#print_hello(3,4,5)
+
+# def myprint(x):
+#     print("<<<",x,">>>")
+# myprint("Yanko")
+# myprint(input())
+
+#пример
+#def name(a,b,c):
+
+#def sum2(a,b):
+    #x=a+b
+    #return x
+#print(sum2(1,2))
+
+#def ab(y):
+    #x=y**2
+    #return x
+#print(ab(int(input()))
+
+#НЕ РЕШЕНО!!!
+# def fun(x,y):
+#     a=x*y
+#     return a
+# print(int(x=input("Enter:"),y=input("Enter:")))
+
+# def ab(y):
+#     x=y**2
+#     return x
+# print(ab(int(input("y"))))
+
+# print(type(2))
+# print(type(2.0))
+# print(type("x"))
+# print(type(True))
+#str, int, float, bool, dict, list
+
+# def fun(x):
+#     if type(x)==str:
+#         print("string")
+#     elif(type(x))==int:
+#         print("int")
+#     elif(type(x))==float:
+#         print("float")
+#     elif(type(x))==bool:
+#         print("bool")
+#     elif(type(x))==dict:
+#         print("dict")
+#     else:
+#         print("Not type")
+# fun(10)
+
+# def fun(x,y):
+#     a=str(x)+str(y)
+#     return(a)
+# print (fun(3,4))
+
