285641a7234ebc6c1561aa49d1a68fea9f9dc4d8
MalkoAl <alexeymalko@gmail.com>
2019-07-03 21:57:07 +0300

    function

--- a.Malko/Lesson 4.py
+++ a.Malko/Lesson 4.py
@@ -8,13 +8,35 @@
 #     print(name,"=",x())
 # m5("m4",m4)
 
-def ul(item1,item2,item3):
-    return "<ul>\n"+li(item1) + "\n" + li(item2)+li(item3)+"</ul>"
+# def ul(item1,item2,item3):
+#     return "<ul>\n"+li(item1) + "\n" + li(item2)+li(item3)+"</ul>"
 # print(ul("its1","its2","its3"))
 
 
-def li(item):
-    return "<li>"+item+"</li>"
+# def li(item):
+#     return "<li>"+item+"</li>"
 # print(li("item"))
 # print(ul(li("Homework"),ul(li("first"),li("second\n"),li("third\n")),li("final")))
-print(ul('1','2','3'))
+# print(ul('1','2','3'))
+# def ul(item1, item2, item3):
+# # #     wrapped = "<ul>" + item1 + item2 + item3 + "</ul>"
+# # #     return wrapped
+# # #
+# # #
+# # # def li(item):
+# # #     wrapped = "<li>" + item + "</li>"
+# # #     return wrapped
+# # #
+# # #
+# # # print(
+# # #     ul(li("a"),
+# # #        li("b"),
+# # #        ul(li("b1"),
+# # #           li("b2"),
+# # #           li("b3"))))
+x=0
+while x<10:
+    if x==3:
+
+            x=x+1
+print("finish", x)
