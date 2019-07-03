f34f4800dd2762a990df09faa8fac49f8291f962
Mushyk <mushyl.s.m18@gmail.com>
2019-07-03 21:22:04 +0300

    Ничего не менял

--- Mushyk/Def_pass.py
+++ Mushyk/Def_pass.py
@@ -12,7 +12,21 @@
 # x=3
 # a(x)
 
+# def ul(item1,item2,item3):
+#     pass
+# def li(item):
+#     pass
+
 def ul(item1,item2,item3):
-    pass
+    return ("<ul=>"+item1+item2+item3+"</ul>")
+print (ul("M","oo","se"))
+
 def li(item):
-    pass
+    wrapped = "<li>" + item + "</li>"
+    return wrapped
+print(
+    ul(li("a"),
+       li("b"),
+       ul(li("b1"),
+          li("b2"),
+          li("b3"))))
