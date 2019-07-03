4dff60ae007b7612dbddccdcf147687df1482c35
MalkoAl <alexeymalko@gmail.com>
2019-07-03 21:05:27 +0300

    function

--- a.Malko/Lesson 4.py
+++ a.Malko/Lesson 4.py
@@ -1,11 +1,20 @@
-def m4():
-    x=1
-    return x+3
-    print(m4, m4())
+# def m4():
+#     x=1
+#     return x+3
+#     print(m4, m4())
+#
+#
+# def m5(name,x):
+#     print(name,"=",x())
+# m5("m4",m4)
+
+def ul(item1,item2,item3):
+    return "<ul>\n"+li(item1) + "\n" + li(item2)+li(item3)+"</ul>"
+# print(ul("its1","its2","its3"))
 
 
-def m5(name,x):
-    print(name,"=",x())
-m5("m4",m4)
-
-
+def li(item):
+    return "<li>"+item+"</li>"
+# print(li("item"))
+# print(ul(li("Homework"),ul(li("first"),li("second\n"),li("third\n")),li("final")))
+print(ul('1','2','3'))
