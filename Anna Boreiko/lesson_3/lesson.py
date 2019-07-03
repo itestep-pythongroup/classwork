df3fc369e18381cbe47bed97eb9fa5eb12d44d38
AnnBoreiko <annboreikoo@gmail.com>
2019-07-03 19:13:15 +0300

    Урок 2

---
+++ Anna Boreiko/lesson_3/lesson.py
@@ -0,0 +1,55 @@
+def print_hello(a,b,c):
+    x=a*b*c
+    print (x)
+print_hello(3,4,5)
+pass
+
+
+#def myprint(WEt):
+    #print ("**",WEt,"**")
+#myprint ("Anna")
+
+
+#def name(a,b):
+#    x=a+b
+#    return x
+#print (name(3,4))
+
+#def name(y):
+  #  x=y**2
+ #   return (x)
+#print(name(int(input())))
+
+# def qwe(a,b):
+#     x=a*b
+#     print (type"x")
+#     return(x)
+# print (qwe(int(input("Enter")),int(input("Enter"))))
+
+# def qwe(a,b):
+#     x=a*b
+#     print (type(347))
+#     return(x)
+# print (type("x"))
+
+# def wer (x):
+#     if (type(x)==str):
+#         print ("False")
+#     elif (type(x)==float):
+#         print ("False")
+#     elif (type(x)==float):
+#         print ("True")
+#     else:
+#         print("Not right")
+#
+# wer("cfd")
+
+
+def wer (x,y):
+    sum2 = str(x)+str(y)
+    return int(sum2)
+
+x = wer(3,4)
+print(x)
+print(type(x))
+
