f45f2bd361676a70ece9b2b70f454e4288a95eca
SergeyLazorenko <52254659+SergeyLazorenko@users.noreply.github.com>
2019-07-07 10:36:33 +0300

    DZ_4

---
+++ S_Lazorenko/DZ_4.py
@@ -0,0 +1,18 @@
+def ol(item1, item2, item3):
+    wrapped = "<ol>" + item1 + item2 + item3 + "</ol>"
+    return wrapped
+
+
+def p(item):
+    wrapped = "<p>" + item + "</p>"
+    return wrapped
+
+
+print(
+    ol(p("привет"),
+       p("еще привет"),
+       ol(p("сам привет"),
+          p("сама привет"),
+          p("все с приветом"))
+       )
+)
