9c4732fc419c16f17b116458d4b0c5f8639e8562
Fixafazatron <lavruhin62>
2019-07-09 23:38:47 +0300

    5_1,5_2,5_3,5_4,5_5,5_6 homework

--- Lavruhin/Homework/homework5_4.py
+++ Lavruhin/Homework/homework5_4.py
@@ -6,3 +6,13 @@
 # ["1","if","2","3","blab","false","345.12"]
 # => ["1","2","3","345.12"]
 # => ["if","blab","false"]
+x=["1", "if", "2", "3", "blab", "false", "345.12"]
+y=[]
+z=[]
+for i in x:
+    try:
+        y.append(float(i))
+    except:
+        z.append(i)
+print(y)
+print(z)
