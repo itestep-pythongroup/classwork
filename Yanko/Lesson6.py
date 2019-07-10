a644cfa45906b80f3d71b150343593d6db3358e7
Yanko <wereshchagin@gmail.com>
2019-07-10 21:49:18 +0300

    Хеш

---
+++ Yanko/Lesson6.py
@@ -0,0 +1,40 @@
+# СТРОКИ
+# юникод. каждый символ имеет свой код
+
+
+# def hello():
+#     print("Hello \U0001f600") # U0001f600 - код символа в юникоде # \ - интерпритатор, экранирование
+#     print(r"Hello \U0001f600") # r - сырая строка
+#     print("Hello \\U0001f600")  # аналогично сырой строке
+#     print(ord("\U0001f600")) # ord - функция которая показывает позицию символа в юникоде (десятичное значение)
+#     print(chr(ord("\U0001f600")))  #
+#     print(ord("a"))
+# hello()
+# print("XVXC Ы BV".encode("utf8").decod("ascii")) # кодирование/декодирование строк
+# print("XVXC Ы BV".encode("utf8").decod("ascii","replace")) # заменить неизвестный символ
+# print("XVXC Ы BV".encode("utf8").decod("ascii","ignore")) # игнорировать неизвестный символ
+# print("XVXC Ы BV")
+
+
+#РЕГУЛЯРКИ
+
+# import re # re - модуль с регулярными выражениями
+# regex=re.compile("[A-z]+@[A-z]+\.com")
+# print(bool(regex.match("agsdjhf"))) #match - достать почту из списка\что-то с вэб страницы
+# print(bool(regex.match("agsdjhfas@gsdge.com")))
+
+# x="привет"
+# x.capitalize()
+
+# #Хеши  пароли
+# print(12312414124%13)#=>10
+# print(10%13)#=>10
+# print(hash("ftgjt"))
+
+
+x=input("Создайте пароль:")
+a=hash(input)
+y=input("Введите пароль:")
+b=hash(y)
+if a==b:
+    print("Yes")
