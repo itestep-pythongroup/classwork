c93c93f52b8823ad91d004ceaafaa7b17de0d527
a.Malko <alexeymalko@gmail.com>
2019-07-13 22:18:11 +0300

    change

--- a.Malko/Homework_5.py
+++ a.Malko/Homework_5.py
@@ -1,8 +1,24 @@
-def f(x):
- y=[]
- while x>1:
-     # y*=x
-     x-=1
-     y.append(x)
- return y
-print (f(11))
+# def f(x):
+#  y=[]
+#  while x>1:
+#      # y*=x
+#      x-=1
+#      y.append(x)
+#  return y
+# print (f(11))
+# y=[]
+# for i in reversed(range(11)):
+#     y.append(i)
+# print(y)
+# x=0
+# y=[]
+# def a(x)
+#     while a<5:
+#     a+=1
+#     while x<5:
+#         x+=1
+#         y.append(0)
+#
+# print( a(5),y)
+print([x for x in range(10, -1, -1)])
+print([[0]*5]*5)
